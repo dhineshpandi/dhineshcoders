1dd16d71aa0d332b8a5862266f983f030ecb6bbe
dhineshpandi <30343013+dhineshpandi@users.noreply.github.com>
2017-07-23 09:55:37 +0530

    Create siva.c

---
+++ siva.c
@@ -0,0 +1,31 @@
+
+This repository
+#include <stdio.h>
+
+void main()
+{
+    int year;
+
+    printf("Enter a year: ");
+    scanf("%d",&year);
+
+    if(year%4 == 0)
+    {
+        if( year%100 == 0)
+        {
+          
+           if ( year%400 == 0)
+                printf("%d is a leap year.", year);
+            else
+                printf("%d is not a leap year.", year);
+        }
+        else
+            printf("%d is a leap year.", year );
+    }
+    else
+        printf("%d is not a leap year.", year);
+    
+    return 0;
+}
+
+
